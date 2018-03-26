d1d6e113db57450c9b58db7b0dd266184c9afdb4
Floryne <floryne.tourret.pro@gmail.com>
2018-03-26 11:43:19 +0200

    Version 0.0 du jeu

--- src/Controller/PartieController.php
+++ src/Controller/PartieController.php
@@ -22,14 +22,10 @@ class PartieController extends Controller
     {
         //Récupère tous les joueurs
         //$joueurs = $this->getDoctrine()->getRepository(User::class)->findAll();
-
         //Récupère les joueurs actifs
         $joueurs=  $this->getDoctrine()->getRepository(User::class)->findBy(['actif' => 1]);
-
         return $this->render('Partie/nouvelle.html.twig', ['joueurs' => $joueurs]);
     }
-
-
     /**
      * @Route("/creer", name="creer_partie")
      */
@@ -41,16 +37,12 @@ class PartieController extends Controller
         } else {
             $id = "Pas d'Id";
         }
-
-
         $idAdversaire = $request->request->get('adversaire');
         $joueur = $this->getDoctrine()->getRepository(User::class)->find($id);
         $adversaire = $this->getDoctrine()->getRepository(User::class)->find($idAdversaire);
-
         //récupérer les objectifs depuis la base de données
         $objectifs = $this->getDoctrine()->getRepository(Objectifs::class)->findAll();
         $tObjectifs = array();
-
         //récupérer les Objets depuis la base de données
         $Objets = $this->getDoctrine()->getRepository(Objets::class)->findAll();
         $tObjets = array();
@@ -58,17 +50,14 @@ class PartieController extends Controller
         foreach ($objectifs as $objectifs) {
             $tObjectifs[] = $objectifs->getId();
         }
-
         //Affecter des valeurs pour les différentes propriétés de partie
         //mélanger les Objets
         foreach ($Objets as $carte) {
             $tObjets[] = $carte->getId();
         }
         shuffle($tObjets); //mélange le tableau contenant les id
-
         //retrait de la première carte
         $carte_jetee = array_pop($tObjets);
-
         //Distribution des Objets aux joueurs,
         $main_j1 = array();
         for ($i = 0; $i < 6; $i++) {
@@ -78,15 +67,12 @@ class PartieController extends Controller
         for ($i = 0; $i < 6; $i++) {
             $tMainJ2[] = array_pop($tObjets);
         }
-
         //La création de la pioche
         $partie_pioche = $tObjets; //sauvegarde des dernières Objets dans la pioche
         //Initialiser actions à 0
         $actions=array(0,0,0,0);
         //Initialiser objectifs à 0
         $objectifs=array(0,0,0,0,0,0,0);
-
-
         //créer un objet de type Partie
         $partie = new Parties();
         $partie->setPartieNom('Cosmos');
@@ -106,18 +92,13 @@ class PartieController extends Controller
         $partie->setScoreJ2(0);
         $partie->setPartiePioche(json_encode(($partie_pioche)));
         $partie->setPartieObjectifs(json_encode(($tObjectifs)));
-
         //Récupérer le manager de doctrine
         $em = $this->getDoctrine()->getManager();
-
         //Sauvegarde mon objet Partie dans la base de données
         $em->persist($partie);
         $em->flush();
-
         return $this->redirectToRoute('afficher_partie', ['id' => $partie->getId()]);
     }
-
-
     /**
      * @Route("/afficher/{id}", name="afficher_partie")
      */
@@ -125,50 +106,38 @@ class PartieController extends Controller
     {
         $Objets = $this->getDoctrine()->getRepository("App:Objets")->findAll();
         $objectifs = $this->getDoctrine()->getRepository("App:Objectifs")->findAll();
-
         $tObjets = array();
         foreach ($Objets as $carte) {
             $tObjets[$carte->getId()] = $carte;
         }
-
         $tObjectifs = array();
         foreach ($objectifs as $objectifs) {
             $tObjectifs[$objectifs->getId()] = $objectifs;
         }
-
         return $this->render('Partie/afficher.html.twig', ['partie' => $partie, 'Objets' =>$tObjets, 'objectifs' =>$tObjectifs]);
     }
-
-
     /**
      * @Route("/rejoindre", name="rejoindre_partie")
      */
     public function rejoindrePartie()
     {
-
         $user = $this->getUser();
         if($user) {
             $id = $user->getId();
         } else {
             $id = "Pas d'Id";
         }
-
         //Récupère les parties avec le joueur dedans
         $parties=  $this->getDoctrine()->getRepository(Parties::class)->findBy(['joueur2' => $id]);
-
         return $this->render('Partie/rejoindre.html.twig', ['parties' => $parties]);
-
     }
-
     /**
      * @Route("/lancer", name="lancer_partie")
      */
     public function lancerPartie(Request $request)
     {
-
         $partie_recup = $request->request->get('id');
         $partie = $this->getDoctrine()->getRepository(Parties::class)->find($partie_recup);
         return $this->redirectToRoute('afficher_partie', ['id' => $partie->getId()]);
-
     }
 }
